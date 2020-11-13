ad56408eea7ab668b7da63eedde0c9fe3983937d
Nikolay Elkinov <54358861+miur-creator@users.noreply.github.com>
2020-11-13 17:40:10 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyAHEjbIc2nPTlbN2iqXRyZv9wgx2zNJSRs",
+  authDomain: "main-presence-208909.firebaseapp.com",
+  databaseURL: "https://main-presence-208909.firebaseio.com",
+  projectId: "main-presence-208909",
+  storageBucket: "main-presence-208909.appspot.com",
+  messagingSenderId: "37240407512",
+  appId: "1:37240407512:web:1991e123c95f61ae025ead",
+};
