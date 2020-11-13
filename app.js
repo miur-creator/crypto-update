a423eff7585e44de0150aef622e88ab0c1750d5b
Nikolay Elkinov <nikolai.elkinov@gmail.com>
2020-11-13 20:38:29 +0300

    background added

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-function app(params) {
-    return
-    const auth = firebase.auth()
-    let loginButtonElement = document.querySelector("#login-button")
-    loginButtonElement.addEventListener('click', onUserLogin)
+// function app(params) {
+//     return
+//     const auth = firebase.auth()
+//     let loginButtonElement = document.querySelector("#login-button")
+//     loginButtonElement.addEventListener('click', onUserLogin)
 
-    function onUserLogin(e) {
-        let usernameElement = document.querySelector("#username")
-        let passwordElement = document.querySelector("#password")
+//     function onUserLogin(e) {
+//         let usernameElement = document.querySelector("#username")
+//         let passwordElement = document.querySelector("#password")
 
-        auth.signInWithEmailAndPassword(usernameElement.value, passwordElement.value)
-            .then(res => {
-                console.log('Looged in succsessfuly')
-            })
-            .catch(console.log('err loggin in'))
-    }
+//         auth.signInWithEmailAndPassword(usernameElement.value, passwordElement.value)
+//             .then(res => {
+//                 console.log('Looged in succsessfuly')
+//             })
+//             .catch(console.log('err loggin in'))
+//     }
 
-}
+// }
 
 
 
-function forexPairProvider() {
-    const url = `https://api.ratesapi.io/api/latest`
-    const forexElement = document.getElementById(`#content-div`)
+// function forexPairProvider() {
+//     const url = `https://api.ratesapi.io/api/latest`
+//     const forexElement = document.getElementById(`#content-div`)
 
-    c
-}
+//     c
+// }
