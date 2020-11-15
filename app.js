7f1e2549868294ea64dda6c7db072f8dc1ed02cc
Nikolay Elkinov <nikolai.elkinov@gmail.com>
2020-11-15 03:20:32 +0300

    trending table part 1

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
+var trendingCoins = []
+
 function forexPairProvider() {
-    const url = `https://www.freeforexapi.com/api/live`
+    const url = `https://api.coingecko.com/api/v3/search/trending.json`
     const forexElement = document.getElementById(`#content-div`)
 
-    console.log(`the function has been invoked`)
+    console.log(`the fetch function has been invoked`)
+    fetch(url)
+        .then(res => res.json())
+        .then(res => res.coins)
+        .then(res => res.map(x => trendingCoins.push([x.item.name, x.item.symbol, x.item.thumb, x.item[`market_cap_rank`]])))
+        console.log(trendingCoins)
+}
+
+function trendingCoinsTemplate(params) {
+
 }
 
 function onUserLogin() {
+    console.log(`the login function has been invoked`)
       let usernameElement = document.querySelector("#username")
       let passwordElement = document.querySelector("#password")
 
@@ -15,4 +27,5 @@ function onUserLogin() {
           document.querySelector("#welcome-message").innerHTML = `Welcome ${res.user.email}`
           document.querySelector("body > div > div.navbar > div > button").parentElement.removeChild(document.querySelector("body > div > div.navbar > div > button"))
         })
+        .catch(err => console.log(err))
     }
